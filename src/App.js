4a448a3fce1281dc98f95bee593bdb6381b840e2
raihan-cse <raihancse46@gmail.com>
2022-05-01 01:07:48 +0600

    input value and submit behaviour done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
+import { useState } from 'react';
 import './App.css';
 import Form from './components/Form';
 import TodoList from './components/TodoList';
 
 function App() {
+  const [inputText, setInputText] = useState('');
   return (
     <div className="todo_app">
-     <header>
-       <h1 className="title">todo</h1>
-     </header>
-     <Form />
-     <TodoList />
+      <header>
+        <h1 className="title">todo</h1>
+      </header>
+      <Form
+        inputText={inputText}
+        setInputText={setInputText}
+      />
+      <TodoList />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,33 +1,43 @@
 import React from 'react'
 import PlusIcon from '../assets/icons/plus.svg'
 
-function Form() {
-  return (
-    <div className='form_wrapper'>
-    <form>
-        <div className="input_group">
-            <input 
-            type="text" 
-            placeholder='Write list here...'
-            className='input_box' 
-            />
-            <button type='submit' className='btn_submit'>
-                <img src={PlusIcon} alt="icon" />
-            </button>
+function Form({ inputText, setInputText }) {
+    const inputTextHandler = (e) => {
+        setInputText(e.target.value)
+    }
+
+    const submitItemHandler = (e) => {
+        e.preventDefault();
+    }
+
+    return (
+        <div className='form_wrapper'>
+            <form>
+                <div className="input_group">
+                    <input
+                        type="text"
+                        placeholder='Write list here...'
+                        className='input_box'
+                        value={inputText}
+                        onChange={inputTextHandler}
+                    />
+                    <button type='submit' className='btn_submit' onClick={submitItemHandler}>
+                        <img src={PlusIcon} alt="icon" />
+                    </button>
+                </div>
+                <div className="select_box">
+                    <div className="label_box">
+                        <span>filter</span>
+                    </div>
+                    <select className='select'>
+                        <option value="all">All</option>
+                        <option value="completed">Completed</option>
+                        <option value="uncompleted">Uncompleted</option>
+                    </select>
+                </div>
+            </form>
         </div>
-        <div className="select_box">
-            <div className="label_box">
-                <span>filter</span>
-            </div>
-            <select className='select'>
-                <option value="all">All</option>
-                <option value="completed">Completed</option>
-                <option value="uncompleted">Uncompleted</option>
-            </select>
-        </div>
-    </form>
-    </div>
-  )
+    )
 }
 
 export default Form
